ad0eead68187b50432e99df12a3c0aa8638dffa7
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2021-06-10 12:14:12 +0530

    Update gdriveTools.py

--- bot/helper/drive_utils/gdriveTools.py
+++ bot/helper/drive_utils/gdriveTools.py
@@ -147,25 +147,25 @@ class GoogleDriveHelper:
                     
                     if x:
                         if add_title_msg == True:
-                            msg = f'<h3>Sᴇᴀʀᴄʜ Rᴇsᴜʟᴛs Fᴏʀ Yᴏᴜʀ Kᴇʏᴡᴏʀᴅ : {fileName}</h3><br><b><a href="https://github.com/iamLiquidX/SearchX"> Bot Repo </a></b> ||<b><a href="https://t.me/imLiquidX"> Owner </a></b><br><br>'
+                            msg = f'<h3>𝙎𝙚𝙖𝙧𝙘𝙝 𝙍𝙚𝙨𝙪𝙡𝙩𝙨: {fileName}</h3><br><b><a href="https://t.me/MaxxBots">𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a></b> ||<b><a href="https://t.me/TGFilmZone"> 𝗣𝗢𝗪𝗥𝗘𝗗 𝗕𝗬 </a></b><br><br>'
                             add_title_msg = False
                         if add_drive_title == True:
                             msg += f"╾────────────╼<br><b>{DRIVE_NAME[INDEX]}</b><br>╾────────────╼<br>"
                             add_drive_title = False
                         if file.get('mimeType') == "application/vnd.google-apps.folder":  # Detect Whether Current Entity is a Folder or File.
                             msg += f"🗃️<code>{file.get('name')}</code> <b>(folder)</b><br>" \
-                                   f"<b><a href='https://drive.google.com/drive/folders/{file.get('id')}'>G-Dʀɪᴠᴇ Lɪɴᴋ</a></b>"
+                                   f"<b><a href='https://drive.google.com/drive/folders/{file.get('id')}'>𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞</a></b>"
                             if INDEX_URL[INDEX] is not None:
                                 url_path = "/".join([requests.utils.quote(n, safe='') for n in self.get_recursive_list(file, parent_id)])
                                 url = f'{INDEX_URL[INDEX]}/{url_path}/'
-                                msg += f'<b> | <a href="{url}">Iɴᴅᴇx Lɪɴᴋ</a></b>'
+                                msg += f'<b> | <a href="{url}">𝗜𝗡𝗗𝗘𝗫 𝗟𝗜𝗡𝗞</a></b>'
                         else:
                             msg += f"<code>{file.get('name')}</code> <b>({self.get_readable_file_size(file.get('size'))})</b><br>" \
-                                   f"<b><a href='https://drive.google.com/uc?id={file.get('id')}&export=download'>G-Dʀɪᴠᴇ Lɪɴᴋ</a></b>"
+                                   f"<b><a href='https://drive.google.com/uc?id={file.get('id')}&export=download'>𝗚-𝗗𝗥𝗜𝗩𝗘 𝗟𝗜𝗡𝗞</a></b>"
                             if INDEX_URL[INDEX] is not None:
                                 url_path = "/".join([requests.utils.quote(n, safe ='') for n in self.get_recursive_list(file, parent_id)])
                                 url = f'{INDEX_URL[INDEX]}/{url_path}'
-                                msg += f'<b> | <a href="{url}">Iɴᴅᴇx Lɪɴᴋ</a></b>'
+                                msg += f'<b> | <a href="{url}">𝗜𝗡𝗗𝗘𝗫  𝗟𝗜𝗡𝗞</a></b>'
                         msg += '<br><br>'
                         content_count += 1
                     if (content_count==TELEGRAPHLIMIT):
@@ -183,7 +183,7 @@ class GoogleDriveHelper:
             self.telegraph_content.append(msg)
             
         if len(self.telegraph_content) == 0:
-            return "<b>➼Nᴏ Rᴇsᴜʟᴛs Fᴏᴜɴᴅ Fᴏʀ Yᴏᴜʀ Fɪʟᴇ Nᴀᴍᴇ Kᴇʏᴡᴏʀᴅ :(</b>", None
+            return "<code>No Results Found 👀</code>", None
       
             
         
@@ -196,11 +196,11 @@ class GoogleDriveHelper:
         if self.num_of_path > 1:
             self.edit_telegraph()
 
-        msg = f" <b>Sᴇᴀʀᴄʜ Rᴇsᴜʟᴛs Fᴏʀ Yᴏᴜʀ Kᴇʏᴡᴏʀᴅ :</b> ➼ {fileName} 👇 "
+        msg = f" <b>Your Search Results:</b> ➼ {fileName} 👇 "
         
-        msg = f" <b>I Have Found  : {content_count} Results For Your Search Query</b> "
+        msg = f" <b>Founded :</b> <code>{content_count}</code> <b>Results For Your Search Query</b></b> "
         
         buttons = button_builder.ButtonMaker()   
-        buttons.buildbutton("Click Here For Results", f"https://telegra.ph/{self.path[0]}")
+        buttons.buildbutton("📦 Click Here to Get", f"https://telegra.ph/{self.path[0]}")
 
         return msg, InlineKeyboardMarkup(buttons.build_menu(1))
